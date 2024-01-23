6fe817eac9397a507c1a3ba08b2da4277454ba7d
saurabh <saurabhkumar0184@gmail.com>
2024-01-23 18:25:55 +0530

    add user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        fullname: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: string,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
