58ea790e872dbb530062c145c603ab6ba9865e9c
saurabh <saurabhkumar0184@gmail.com>
2024-01-26 18:04:48 +0530

    hash user password

--- src/models/user.js
+++ src/models/user.js
@@ -21,11 +21,33 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
         },
         password: {
-            type: string,
+            type: String,
             required: true,
         },
     },
     { timestamps: true }
 );
+
+// Middleware: Hash the password before saving (pre-save hook)
+userSchema.pre("save", async function (next) {
+    try {
+        if(this.isModified("password")) {
+            this.password = await bcrypt.hash(this.password, 8);
+            next();
+        } else {
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+// check if a given password is correct
+userSchema.methods.isPasswordCorrect = async function (password)
+{
+    return await bcrypt.compare(password, this.password)
+}
+
 
 export const User = mongoose.model('User', userSchema);
