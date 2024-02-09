e38da64ea3082ea114df33d2cb1047b1ad073758
saurabh <saurabhkumar0184@gmail.com>
2024-02-09 10:35:26 +0530

    add addProduct controller

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,41 @@
+import {Product} from "../models/product.model.js";
+import {Category} from "../models/category.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { cloudinaryUpload, cloudinaryDelete } from "../utils/cloudinary.js";
+
+const addProduct = asyncHandler(async(req, res) => {
+
+    const {name, description, price, category, stock} = req.body
+
+    const getCategory = await Category.findById(category);
+
+    if(!getCategory) {
+        throw new ApiError(402, "Category doesn't exist")
+    }
+
+    const productImagePath = req.files?.productImage[0].path;
+
+    const productImageUrl = await cloudinaryUpload(productImagePath) 
+
+    if(!productImageUrl) {
+        throw new ApiError(400, "Failed to upload product image")
+    }
+
+    const product = await Product.create({
+        name,
+        description,
+        price,
+        stock,
+        productImage: productImageUrl
+    })
+
+    return res.status(200)
+    .json(
+        new ApiResponse(201, product, "Product created successfully")
+    )
+})
+
+
+export {addProduct}
