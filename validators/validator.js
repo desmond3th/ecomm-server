3aa69fb0e880ad8f547794345d2f727cb3b33c29
saurabh <saurabhkumar0184@gmail.com>
2024-02-12 12:58:12 +0530

    configure validator for product controller, configure mongodb validator

--- validators/validator.js
+++ validators/validator.js
@@ -1,7 +1,7 @@
 import { validationResult } from "express-validator";
 import { ApiError } from "../utils/ApiError.js";
 
-export const validate = (req, res, next) => {
+export const validate = (req, _, next) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
         return next();
