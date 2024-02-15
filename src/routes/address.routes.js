1bc905a6ba720b0dac2cb2a844c36e7378d48dbd
saurabh <saurabhkumar0184@gmail.com>
2024-02-15 19:43:10 +0530

    add routes for category controllers

--- src/routes/address.routes.js
+++ src/routes/address.routes.js
@@ -11,8 +11,10 @@ const router = new Router();
 router.use(verifyJWT)
 
 router.route("/add-address").post(addAddressValidator(), validate, addAddress);
-router.route("/update-address").patch(updateAddressValidator(), validate, updateAddress);
 router.route("/get-addresses").get(getAllAddresses);
+
+router.route("/update-address/:addressId")
+    .patch(updateAddressValidator(), mongoIdPathVariableValidator("addressId"), validate, updateAddress);
 router.route("/delete-address/:addressId").delete(mongoIdPathVariableValidator("addressId"), validate, deleteAddress);
 router.route("get-address-by-id/:addressId").get(mongoIdPathVariableValidator("addressId"), validate, getAdressById);
 
