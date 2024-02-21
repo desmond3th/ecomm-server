13ea37fc7c483fa57500f5f8260271461e5808c8
saurabh <saurabhkumar0184@gmail.com>
2024-02-21 18:51:24 +0530

    add orderConfirmationMailgenContent

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -85,6 +85,42 @@ const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
     };
 };
 
+
+const orderConfirmationMailgenContent = (username, items, totalCost) => {
+    return {
+        body: {
+            name: username,
+            intro: "Your order has been processed successfully.",
+            table: {
+                data: items?.map((item) => {
+                    return {
+                        item: item.product?.name,
+                        price: "INR " + item.product?.price + "/-",
+                        quantity: item.quantity,
+                    };
+                }),
+                columns: {
+                    customWidth: {
+                        item: "20%",
+                        price: "15%",
+                        quantity: "15%",
+                    },
+                    customAlignment: {
+                        price: "right",
+                        quantity: "right",
+                    },
+                },
+            },
+            outro: [
+                `Total order cost: INR ${totalCost}/-`,
+                "You can check the status of your order and more in your order history",
+            ],
+        },
+    };
+};
+
+
 export { sendEmail,
         emailVerificationMailgenContent,
-        forgotPasswordMailgenContent}
+        forgotPasswordMailgenContent,
+        orderConfirmationMailgenContent}
